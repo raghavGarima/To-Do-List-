4e1478ea6a4a77c30a94b4f67ed943f790946056
Garima Raghav <garimaraghav8@gmail.com>
2022-05-12 13:00:45 +0530

    First Project Angular 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TopComponent } from './component/top/top.component';
-import { BodyComponent } from './component/bottom/body/body.component';
 @NgModule({
   declarations: [
     AppComponent,
     TopComponent,
-    BodyComponent
+    
   ],
   imports: [
     BrowserModule
